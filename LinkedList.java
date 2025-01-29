6423581ae0032b2fac5674ff64f51d6a81b56d9a
1U23CS045JEROBINNIXSON <139536670+1U23CS045JEROBINNIXSON@users.noreply.github.com>
2025-01-29 07:08:45 +0000

    submit

--- LinkedList.java
+++ LinkedList.java
@@ -72,11 +72,27 @@ public static void main(String[] args) {
            }
         }
     }
-public void insertMiddle() 
-    {    
-//TYPE YOUR CODE HERE
+public void insertMiddle(int data, int pos) {  
+    Node newNode = new Node(data);
+    if (pos == 1) 
+    {  
+        newNode.next = head;
+        head = newNode;
+        return;
     }
-
+    Node temp = head;
+    for (int i = 1; i < pos - 1 && temp != null; i++) 
+    {  
+        temp = temp.next;
+    }
+    if (temp == null) 
+    {  
+        System.out.println("Invalid position");
+        return;
+    }
+    newNode.next = temp.next; 
+    temp.next = newNode;
+}
 public void displayList() {
         Node current = head;
         while (current != null) {
